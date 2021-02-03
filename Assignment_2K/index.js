14a135f023a4974697d6777419c4661da267720f
kristofergauti <kristofergauti@gmail.com>
2021-02-03 23:50:32 +0000

    Cards frontend functionality finised, but needs testing

--- Assignment_2K/index.js
+++ Assignment_2K/index.js
@@ -1,5 +1,44 @@
-let bergur = document.getElementById("kristofer");
+let mainTag = document.getElementsByTagName("main");
+let formTag = document.getElementsByClassName("theForm");
+let data = new URLSearchParams();
 
-bergur.addEventListener("click", function () {
-    console.log("Hi Kristofer!");
-});
+function getFormData(){
+    // (A) get form
+    
+    
+    // // (B) AJAX
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("GET", "boards.html" + data.toString());
+    // // What to do when server responds
+    // xhr.onload = function(){ console.log(this.response); };
+    // xhr.send();
+    
+
+    // (C) PREVENT HTML FORM SUBMIT
+    
+}
+
+function insertCard(){
+    data.append("boardInput", document.getElementById("boardInput").value);
+
+    let divTag = document.createElement("DIV");
+    let titleTag = document.createElement("P");
+    titleTag.setAttribute("class", "title")
+    divTag.setAttribute("class", "card");
+    for (let inputDataList of data) {
+        if (inputDataList[1] != "") {
+            titleTag.innerHTML = inputDataList[1];
+            divTag.appendChild(titleTag);
+            mainTag[0].appendChild(divTag);
+        }
+    }
+    
+    //To prevent reloading the page
+    return false;
+}
+
+function executeEventListeners() {
+    
+}
+
+executeEventListeners()
