64d107202aa0f5ef3178415bd74d17e9bc210d6e
kristofer <kristofergauti@gmail.com>
2021-02-08 01:11:19 +0000

    modifyed the cards and added a create a task input form, it is a bit buggy

--- Assignment_2K/index.js
+++ Assignment_2K/index.js
@@ -1,44 +1,80 @@
 let mainTag = document.getElementsByTagName("main");
-let formTag = document.getElementsByClassName("theForm");
-let data = new URLSearchParams();
+let Inputs = document.getElementsByClassName("Input");
 
-function getFormData(){
-    // (A) get form
-    
-    
-    // // (B) AJAX
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("GET", "boards.html" + data.toString());
-    // // What to do when server responds
-    // xhr.onload = function(){ console.log(this.response); };
-    // xhr.send();
-    
+function loadAllCards(){
+    /**
+     * Backend stuff finish later
+     */
+    let url = "https://veff-boards-h1.herokuapp.com/api/v1/boards";
 
-    // (C) PREVENT HTML FORM SUBMIT
-    
+    axios.get(url)
+    .then((response) => {
+        // for (board of response.data) {
+        //     console.log(board.name);
+        // }
+    }).catch((error) => {console.log("ERROR MY GUY", error)});
 }
 
 function insertCard(){
-    data.append("boardInput", document.getElementById("boardInput").value);
-
-    let divTag = document.createElement("DIV");
-    let titleTag = document.createElement("P");
-    titleTag.setAttribute("class", "title")
-    divTag.setAttribute("class", "card");
-    for (let inputDataList of data) {
-        if (inputDataList[1] != "") {
-            titleTag.innerHTML = inputDataList[1];
-            divTag.appendChild(titleTag);
-            mainTag[0].appendChild(divTag);
-        }
-    }
     
     //To prevent reloading the page
     return false;
 }
-
-function executeEventListeners() {
-    
+function insertTask(){
+    return false;
 }
 
-executeEventListeners()
+function createCard() {
+    let divTag = document.createElement("div");
+    let deleteBtn = document.createElement("div");
+    let titleTag = document.createElement("p");
+    let taskForm = document.createElement("form");
+    let taskInput = document.createElement("input");
+    
+    divTag.setAttribute("class", "card");
+    deleteBtn.setAttribute("class", "Ex");
+    titleTag.setAttribute("class", "title");
+    taskForm.setAttribute("onsubmit", "return insertTask();");
+    taskForm.setAttribute("autocomplete", "off");
+    taskForm.setAttribute("method", "GET");
+    taskInput.setAttribute("type", "text");
+    taskInput.setAttribute("class","Input taskInput");
+    taskInput.setAttribute("name","taskInput");
+    taskInput.style.marginLeft = "0"
+    taskInput.setAttribute("placeholder","Create new task");
+    titleTag.innerText = Inputs[0].value;
+
+
+    divTag.appendChild(deleteBtn);
+    divTag.appendChild(titleTag);
+    divTag.appendChild(taskForm);
+    taskForm.appendChild(taskInput);
+    mainTag[0].appendChild(divTag);
+
+    executeEventListeners([Inputs[0], taskInput]);
+    //To prevent reloading the page
+    return false;
+}
+
+function createTask(){
+    console.log("You just created a task")
+}
+
+function executeEventListeners(elementList) {
+    /**
+     * If user creates board and then creates a task and creates 
+     * a board again, everything will flop, fix this!
+     */
+    elementList.forEach((elem) => {
+        elem.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                console.log("enter presssed");
+                if(elem.classList.contains("taskInput")) {
+                    createTask();
+                }
+                elem.value = "";
+            }
+        });
+    })
+}
+
