47f7a8ec92aad4584ccc32f9b32b06200620ceb9
kristofer <kristofergauti@gmail.com>
2021-02-11 01:47:40 +0000

    Some backend implemented, need to order the tasks on right board where board.id == task.id

--- Assignment_2K/index.js
+++ Assignment_2K/index.js
@@ -1,30 +1,33 @@
 let mainTag = document.getElementsByTagName("main");
 let Inputs = document.getElementsByClassName("Input");
 
-function loadAllCards(){
-    /**
-     * Backend stuff finish later
-     */
-    let url = "https://veff-boards-h1.herokuapp.com/api/v1/boards";
+function loadAllBoards(){
+    let boardUrl = "https://veff-boards-h1.herokuapp.com/api/v1/boards";
+    let boardIdList = [];
 
-    axios.get(url)
+    axios.get(boardUrl)
     .then((response) => {
-        // for (board of response.data) {
-        //     console.log(board.name);
-        // }
-    }).catch((error) => {console.log("ERROR MY GUY", error)});
-}
+        for (board of response.data) {
+            boardIdList.push(board.id);
+            createCard(board, true);
+            
+            let taskUrl = `https://veff-boards-h1.herokuapp.com/api/v1/boards/${board.id}/tasks`;
+            axios.get(taskUrl)
+            .then((response) => {
+                console.log(response);
+                let divTag = document.getElementsByClassName("card")[0];
+                for (task of response.data) {
+                    console.log(task.id, board.id);
+                    _createTask(task, divTag, true);
+                }
+            }).catch((error) => {console.log("ERROR! from getting the tasks.", error)});
+        }
+    }).catch((error) => {console.log("ERROR! from getting the boards.", error)});
 
-function insertCard(){
     
-    //To prevent reloading the page
-    return false;
-}
-function insertTask(){
-    return false;
 }
 
-function createCard() {
+function createCard(inputElem, fromBackend) {
     let divTag = document.createElement("div");
     let deleteBtn = document.createElement("div");
     let titleTag = document.createElement("p");
@@ -34,7 +37,7 @@ function createCard() {
     divTag.setAttribute("class", "card");
     deleteBtn.setAttribute("class", "Ex");
     titleTag.setAttribute("class", "title");
-    taskForm.setAttribute("onsubmit", "return insertTask();");
+    taskForm.setAttribute("onsubmit", "return false;");
     taskForm.setAttribute("autocomplete", "off");
     taskForm.setAttribute("method", "GET");
     taskInput.setAttribute("type", "text");
@@ -42,37 +45,44 @@ function createCard() {
     taskInput.setAttribute("name","taskInput");
     taskInput.style.marginLeft = "0"
     taskInput.setAttribute("placeholder","Create new task");
-    titleTag.innerText = Inputs[0].value;
+    if (fromBackend) titleTag.innerText = inputElem.name;
+    else titleTag.innerText = inputElem.value;
     Inputs[0].value = "";
+
     divTag.appendChild(deleteBtn);
     divTag.appendChild(titleTag);
     divTag.appendChild(taskForm);
     taskForm.appendChild(taskInput);
     mainTag[0].appendChild(divTag);
 
-    executeEventListeners([Inputs[0], taskInput], divTag);
+    executeEventListener([Inputs[0], taskInput], divTag);
 
     //To prevent reloading the page
     return false;
 }
 
-function executeEventListeners(elementList, parentElement) {
+function _createTask(task, parentElement, fromBackend) {
+    let deleteBtn = document.createElement("div");
+    let taskDiv = document.createElement("div");
+    let taskParagraph = document.createElement("p");
+
+    taskDiv.setAttribute("class", "task");
+    deleteBtn.setAttribute("class", "Ex");
+    if (fromBackend) taskParagraph.innerText = task.taskName;
+    else taskParagraph.innerText = task.value;
+    taskParagraph.style = "width: 80%;";
+    
+    taskDiv.appendChild(taskParagraph);
+    taskDiv.appendChild(deleteBtn);
+    parentElement.appendChild(taskDiv);
+    event.target.value = "";
+}
+
+function executeEventListener(elementList, parentElement) {
     elementList.forEach((elem) => {
         elem.addEventListener("keypress", (event) => {
             if (event.key === "Enter" && elem.classList.contains("taskInput")) {
-                let deleteBtn = document.createElement("div");
-                let taskDiv = document.createElement("div");
-                let taskParagraph = document.createElement("p");
-
-                taskDiv.setAttribute("class", "task");
-                deleteBtn.setAttribute("class", "Ex");
-                taskParagraph.innerText = event.target.value;
-                taskParagraph.style = "width: 80%;";
-                
-                taskDiv.appendChild(taskParagraph);
-                taskDiv.appendChild(deleteBtn);
-                parentElement.appendChild(taskDiv);
-                event.target.value = "";
+                _createTask(event.target, parentElement, false);
             }
         });
     })
